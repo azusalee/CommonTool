5c9cff2da88c7eb87a09770ed5815ea959c0f115
azusalee <384433472@qq.com>
2019-10-24 16:48:15 +0800

    调整键盘监听代码;

--- CommonTool/Cate/UIView/Swift/UIScrollView+ALKeyboard.swift
+++ CommonTool/Cate/UIView/Swift/UIScrollView+ALKeyboard.swift
@@ -11,29 +11,32 @@ import UIKit
 extension UIScrollView {
     /// 监听键盘，自动调节offset，在viewwillappear調用
     func al_addKeyboardNotification() {
-        //NotificationCenter.default.addObserver(self, selector: #selector(UIScrollView.keyboardWillAppear(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (notification) in
-            if let responseView = self.al_getResponseView(pView: self) {
-                let user_info = notification.userInfo
-                let keyboardRect = (user_info?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-                if let responseRect = responseView.superview?.convert(responseView.frame, to: UIApplication.shared.keyWindow) {
-                    let responseBottomInScrollView = responseRect.origin.y+responseRect.size.height+10
-                    let moveOffset = responseBottomInScrollView-keyboardRect.origin.y;
-                    
-                    if moveOffset > 0 {
-                        var originOffset = self.contentOffset;
-                        originOffset.y += moveOffset;
-                        self.setContentOffset(originOffset, animated: true)
-                    }
-                }
-                
-            }
-        }
+        NotificationCenter.default.addObserver(self, selector: #selector(UIScrollView.al_keyboardWillAppear(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
     }
     
     /// 移除键盘监听，在viewwilldisappear調用
     func al_removeKeyboardNotification() {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    /// 键盘出现
+    @objc
+    func al_keyboardWillAppear(notification: Notification) {
+        if let responseView = self.al_getResponseView(pView: self) {
+            let user_info = notification.userInfo
+            let keyboardRect = (user_info?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+            if let responseRect = responseView.superview?.convert(responseView.frame, to: UIApplication.shared.keyWindow) {
+                let responseBottomInScrollView = responseRect.origin.y+responseRect.size.height+10
+                let moveOffset = responseBottomInScrollView-keyboardRect.origin.y;
+                
+                if moveOffset > 0 {
+                    var originOffset = self.contentOffset;
+                    originOffset.y += moveOffset;
+                    self.setContentOffset(originOffset, animated: true)
+                }
+            }
+            
+        }
     }
     
     /// 獲取正在響應的view
